a723384e50c82eeca83a799bcafe7caa1ec8d8f9
Mahamad Shah <qaiumshah@yahoo.com>
2023-05-09 10:59:38 +0300

    Node crash course initial commit

---
+++ reference/os_demo.js
@@ -0,0 +1,23 @@
+const os = require('os');
+
+//platform
+console.log(os.platform());
+
+//CPU architecture
+console.log(os.arch());
+
+//CPU core info
+console.log(os.cpus());
+
+//Free memory
+console.log(os.freemem());
+
+//total memory
+console.log(os.totalmem());
+
+//Home dir
+console.log(os.homedir());
+
+
+//Uptime of the pc
+console.log(os.uptime());
